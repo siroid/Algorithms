274275127102653f243046fa427983497623cf9f
Igor Siroid <siroid@gmail.com>
2018-08-12 14:12:28 -0700

    first commit

--- ProgrammingAssignments/Program.cs
+++ ProgrammingAssignments/Program.cs
@@ -6,7 +6,64 @@ namespace ProgrammingAssignments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.Write("Enter dimension of array: ");
+            int n = int.Parse(Console.ReadLine());
+            long[] array = new long[n];
+
+            //Console.Write("Enter elements of array: ");
+            string[] strArray = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(strArray[i]);
+            }
+
+            long product = MaxPairwiseProductFast(array);
+            Console.WriteLine(product);
+        }
+
+        static long MaxPairwiseProductNaive(long[] A)
+        {
+            int n = A.Length;
+            long product = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    product = Math.Max(product, A[i] * A[j]);
+                }
+            }
+
+            return product;
+        }
+
+        static long MaxPairwiseProductFast(long[] A)
+        {
+            int n = A.Length;
+            int index1 = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (A[i] > A[index1])
+                {
+                    index1 = i;
+                }
+            }
+            int index2 = 0;
+
+            if (index1 == 0)
+            {
+                index2 = 1;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (A[i] != A[index1] & A[i] > A[index2])
+                {
+                    index2 = i;
+                }
+            }
+
+            return A[index1] * A[index2];
         }
     }
 }
